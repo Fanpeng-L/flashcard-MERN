9b52f80d774ab5a7359ee9dbff8979beec62d65e
Fanpeng Li <serforfp@gmail.com>
2024-03-15 19:50:31 +0000

    connected to mongoose, added dotenv for security

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import Deck from "./models/Deck";
+import { config } from "dotenv";
+config();
 
 const app = express();
 const port = 5000;
@@ -10,12 +12,8 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 // connect to mongoose
-mongoose
-  .connect(
-    "mongodb+srv://flashcarduser:rRKAayJzsuAmNpka@cluster0.sljuu7s.mongodb.net/"
-  )
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
   });
+});
