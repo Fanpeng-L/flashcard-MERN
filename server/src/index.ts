db01b6d330ecbce3ea9022754d6aac97c879e57c
Fanpeng Li <serforfp@gmail.com>
2024-03-15 19:50:06 +0000

    Connected to mongoose, added dotenv for URL security

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck";
+
 const app = express();
 const port = 5000;
 
@@ -6,6 +9,13 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// connect to mongoose
+mongoose
+  .connect(
+    "mongodb+srv://flashcarduser:rRKAayJzsuAmNpka@cluster0.sljuu7s.mongodb.net/"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  });
