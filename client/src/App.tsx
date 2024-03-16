3f1b7b0a855cad22b9e21f99e92ce9e0380ef253
Fanpeng Li <serforfp@gmail.com>
2024-03-16 14:28:59 +0000

    Displayed all GET decks on website

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+
+type TDeck = {
+  title: string;
+  _id: string;
+};
 
 function App() {
   const [title, setTitle] = useState("");
+  const [decks, setDecks] = useState<TDeck[]>([]);
 
   async function createDeckHandler(e: React.FormEvent) {
     e.preventDefault();
@@ -18,11 +24,30 @@ function App() {
     setTitle("");
   }
 
+  useEffect(() => {
+    async function fetchDecks() {
+      const newDecks = await fetch("http://localhost:5000/decks").then(
+        (response) => response.json()
+      );
+      setDecks(newDecks);
+    }
+    fetchDecks();
+  }, []);
+
   return (
     <div className="App">
-      <form onSubmit={createDeckHandler}>
-        <label htmlFor="deck-title">Deck Title</label>
+      <ul className="decks">
+        {decks.map((deck) => (
+          <li key={deck._id}>{deck.title}</li>
+        ))}
+      </ul>
+      <form className="createDeck-form" onSubmit={createDeckHandler}>
+        <label className="createDeck-label" htmlFor="deck-title">
+          Deck Title:
+        </label>
         <input
+          className="createDeck-input"
+          placeholder="Enter your deck title"
           id="deck-title"
           value={title}
           type="text"
@@ -30,7 +55,7 @@ function App() {
             setTitle(e.target.value);
           }}
         />
-        <button>Create Card Deck</button>
+        <button className="createDeck-btn">Create Card Deck</button>
       </form>
     </div>
   );
